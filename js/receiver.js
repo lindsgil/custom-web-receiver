99b7175af08958b89f57dbdfab859bd5a9619691
Lindsay Gilbert <lindsay@artblocks.io>
2024-09-25 09:40:47 -0400

    add message listener

---
+++ js/receiver.js
@@ -0,0 +1,36 @@
+const context = cast.framework.CastReceiverContext.getInstance();
+const playerManager = context.getPlayerManager();
+
+/**
+ * Enable debug log from Google Cast SDK
+ */
+
+context.setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
+
+const iframeContainer = document.getElementById('iframeContainer');
+
+// Custom message namespace
+const NAMESPACE = 'urn:x-cast:com.example.livetokendisplay';
+
+context.addCustomMessageListener(NAMESPACE, function(event) {
+    const url = event.data.url;
+    if (url) {
+        // Create and set up the iframe
+        const iframe = document.createElement('iframe');
+        iframe.src = url;
+        iframe.allow = "autoplay; fullscreen";
+        iframe.style.position = 'fixed';
+        iframe.style.top = '0';
+        iframe.style.left = '0';
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        iframe.style.border = 'none';
+        
+        // Clear previous content and append the new iframe
+        iframeContainer.innerHTML = '';
+        iframeContainer.appendChild(iframe);
+    }
+});
+
+// Start the receiver
+context.start();
